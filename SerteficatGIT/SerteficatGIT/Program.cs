9531211c7ec8c0df52b9e5a21746bd3489fb3e34
yaroslav1martynov <bibasvip@mail.ru>
2016-12-03 23:02:35 +0300

    Start C# hellofichure

---
+++ SerteficatGIT/SerteficatGIT/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerteficatGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string git = "Описание работы C# в GIT";
+        }
+    }
+}
